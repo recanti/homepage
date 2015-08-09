f978eda732dabec5f086d948cafdc36bcf62e72d
Ben W <reccanti@gmail.com>
2015-08-09 03:04:45 -0400

    Began Styling
    Finished writing the about section and began styling the page. Also did work on the gulp file to get resources to properly transfer over.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 'use strict'
 
 var gulp = require('gulp');
+var browsersync = require('browser-sync').create();
 var compass = require('gulp-compass');
 
+// this will compile sass files to css and move them to the build folder
+// afterwards, call browsersync
 gulp.task('compass', function () {
 	return gulp.src('./scss/**/*.scss')
 		.pipe(compass({
@@ -11,6 +14,39 @@ gulp.task('compass', function () {
 			require: ['susy']
 		}))
 		.on('error', function(err) {
-			return;
+			console.log(err);
+		})
+		.pipe(browsersync.stream());
+});
+
+// this will simply move the html to the build folder
+gulp.task('html', function() {
+	return gulp.src(['*.html'])
+		.pipe(gulp.dest('build'))
+		.on('error', function(err) {
+			console.log(err);
 		});
 });
+
+// this will copy the resources folder whenever there is a change
+gulp.task('resources', function() {
+	return gulp.src(['./resources/**/*.*'])
+		.pipe(gulp.dest('build/resources'))
+		.on('error', function(err) {
+			console.log(err);
+		});
+})
+
+// watches for changes and runs the appropriate task
+gulp.task('watch', function() {
+
+	browsersync.init({
+		server: "./build/"
+	});
+
+	gulp.watch('scss/**/*.scss', ['compass']);
+	gulp.watch('resources/**/*.*', ['resources']);
+	gulp.watch('*.html', ['html']).on('change', browsersync.reload);
+});
+
+gulp.task('default', ['compass', 'watch']);
